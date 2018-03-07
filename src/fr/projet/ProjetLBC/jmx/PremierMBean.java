5a7abdef4b1f415d5fff9980044d11310ac89f35
Egel <yoann.mazza1@gmail.com>
2018-03-07 16:31:46 +0100

    Ajout du premier mbean jmx

--- src/fr/projet/ProjetLBC/jmx/PremierMBean.java
+++ src/fr/projet/ProjetLBC/jmx/PremierMBean.java
@@ -2,6 +2,9 @@ package fr.projet.ProjetLBC.jmx;
 
 public interface PremierMBean {
 
-
+    public String getNom();
+    public int getValeur();
+    public void setValeur(int valeur);
+    public void rafraichir();
 
 }
